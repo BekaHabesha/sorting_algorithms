69283900e243a76170f4b4a5881e24cd9cc70915
BekaHabesha <bereketdm1984@gmail.com>
2023-12-19 22:07:00 +0300

    0-bubble_sort.c - N0 0-Bubble sort.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,50 @@
+/*
+* File_name: 0-bubble_sort.c
+* Task_no: 0
+* Task_Title: Bubble sort
+* Functions: void swap_ints, void bubble_sort
+* Created: On December 19, 2023
+* Author: Bereket Dereje Mekonnen
+* GitHub repository: sorting_algorithms
+* Project Title: 0x1B. C - Sorting algorithms & Big O
+* Directory: sorting_algorithms
+*/
+
 #include "sort.h"
 
 /**
- * bubble_sort - Sort an array of integers in ascending order.
+ * swap_ints - It Swap two integers in an array.
+ *
+ * Author: Bereket Dereje Mekonnen.
+ *
+ * @a: The first integer to swap.
+ *
+ * @b: The second integer to swap.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+/**
+ * bubble_sort - It Sort an array of integers
+ *               in ascending order.
+ *
+ * Author: Bereket Dereje Mekonnen.
+ *
  * @array: An array of integers to sort.
+ *
  * @size: The size of the array.
  *
  * Description: Prints the array after each swap.
  */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
@@ -29,23 +67,4 @@ void bubble_sort(int *array, size_t size)
 		}
 		len--;
 	}
-}
-
-/**
- * swap_ints - It Swap two integers in an array.
- *
- * Author: Bereket Dereje Mekonnen.
- *
- * @a: The first integer to swap.
- *
- * @b: The second integer to swap.
- */
-
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
 }
